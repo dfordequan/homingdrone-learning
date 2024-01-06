9226fd215fa8b29b5e22de2811c237a1462e574a
dou <d.ou@student.tudelft.nl>
2024-01-06 14:54:27 +0100

    changed output label

--- data_gym/data_processing.py
+++ data_gym/data_processing.py
@@ -31,16 +31,36 @@ def center_gaze_direction(image, degree, north_col):
 
     return output_image
 
-def get_home_direction(pos_x,pos_y):
-    home_direction_global = np.array([-pos_x, -pos_y])
-    transformation_matrix = np.array([[0, -1], [1, 0]])
-    home_direction_local = np.matmul(transformation_matrix, home_direction_global)
-    home_direction_local = home_direction_local / np.linalg.norm(home_direction_local)
+def get_home_direction(pos_x, pos_y):
+    """
+    input the x,y coordinate with nest at (0,0) and y towards north
 
-    return home_direction_local
+    returns the angle in degrees that the drone have to turn from facing y to origin in clockwise direction
+    
+    """
+
+    angle = np.arccos(-pos_y/np.sqrt(pos_x**2+pos_y**2))
+
+    # if right half plane, convert to degrees and subtract 180
+    if pos_x > 0:
+        return -np.rad2deg(angle)
+    
+    # if left half plane, keep it as it is and convert to degrees
+    else:
+        return np.rad2deg(angle)
+    
+
+def deg_to_unit_vector(deg):
+    """
+    input the angle in degrees
+
+    returns the unit vector in the direction of the angle
+    """
+    return np.array([np.cos(np.deg2rad(deg)), np.sin(np.deg2rad(deg))])
+
 
 def get_relative_home_direction(home_direction, gaze):
-    gaze_matrix = np.array([[np.cos(np.deg2rad(gaze)), np.sin(np.deg2rad(gaze))], [-np.sin(np.deg2rad(gaze)), np.cos(np.deg2rad(gaze))]])
+    gaze_matrix = np.array([[np.cos(np.deg2rad(gaze)), -np.sin(np.deg2rad(gaze))], [np.sin(np.deg2rad(gaze)), np.cos(np.deg2rad(gaze))]])
     relative_home_direction = np.matmul(gaze_matrix, home_direction)
 
     return relative_home_direction
@@ -48,7 +68,7 @@ def get_relative_home_direction(home_direction, gaze):
 
 # save data to a csv file
 output_location = "./data_gym/training/"
-with open("./data_gym/processed.csv", "a") as f:
+with open("./data_gym/processed_label.csv", "a") as f:
     f.write("filename, id, gaze, pos_x, pos_y, label_1, label_2\n")
     for i in range(1,12):
         for j in range(1,12):
@@ -63,23 +83,25 @@ with open("./data_gym/processed.csv", "a") as f:
             image = cv2.imread(file_location + filename + ".jpg", cv2.IMREAD_GRAYSCALE)
 
             #determine the pos_x and pos_y, the grid is 11(A-K) x 11(1-11), the center is (6,6)
-            pos_x = j - 6
-            pos_y = i - 6
+            pos_x = 6 - i
+            pos_y = j - 6
             # skip (0,0)
             if pos_x == 0 and pos_y == 0:
                 continue
             
-            home_direction = get_home_direction(pos_x,pos_y)
+            home_angle_deg = get_home_direction(pos_x,pos_y)
+
+            home_vector = deg_to_unit_vector(home_angle_deg)
 
             
 
 
     
             for k in range(1, 361):
-                image_gaze = center_gaze_direction(image, k, 900)
+                # image_gaze = center_gaze_direction(image, k, 900)
 
                 gaze = k
-                label = get_relative_home_direction(home_direction, gaze)
+                label = get_relative_home_direction(home_vector, gaze)
 
                 # padding k to 3 digits
                 if k < 10:
